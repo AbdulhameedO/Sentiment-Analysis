6267129b5d13a927ec18634d2e137ad88d04335f
Abdulhameed Osama <meedosama2001@gmail.com>
2024-05-22 23:45:28 +0300

    Mongo Connection

--- dependencies/db/client.py
+++ dependencies/db/client.py
@@ -6,7 +6,9 @@ from pymongo import errors as mongo_errors
 from fastapi import HTTPException
 from fastapi import status
 
-
+# Intialize the MONGO_URI with gpteam and MONGO_DB with 12345
+MONGO_URI = "mongodb+srv://gpteam:T1u26RnOuvjrPPv9@gp.o3j9wfq.mongodb.net/"
+MONGO_DB = "gpteam"
 class Client:
     _instance = None
 
@@ -20,8 +22,8 @@ class Client:
         if not Client._instance:
             try:
                 Client._instance = self
-                self.client = MongoClient(os.environ.get("MONGO_URI"))
-                self.db = self.client[os.environ.get("MONGO_DB")]
+                self.client = MongoClient(MONGO_URI)
+                self.db = self.client[MONGO_DB]
             except mongo_errors.PyMongoError:
                 raise HTTPException(detail="database error", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
