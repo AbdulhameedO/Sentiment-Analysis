4b24fa8299914f212318cadd9e26a5ce67897750
SeifAllah Mohamed Kishtah <seifallahmohamedkishtah@SeifAllahs-MacBook-Air.local>
2024-05-31 04:36:47 +0300

    modified homepag.js

--- Website/speax/src/Components/Homepage/Homepage.js
+++ Website/speax/src/Components/Homepage/Homepage.js
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import './Homepage.css'
 import AudioPlay from '../AudioPlayer/audioPl'
 import ProgressBar from '../player/Barpl'
+import { getAudio } from '../../Apiservice'
+import axios from 'axios';
 const Homepage = () => {
   const [inputText, setInputText] = useState('');
   const handleInputChange = (event) => {
@@ -12,6 +14,26 @@ const Homepage = () => {
     event.preventDefault();
     alert(`Input value submitted: ${inputText}`);
   };
+  const handleGetAudio = async () => {
+    try {
+        const response = await getAudio();
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const posttts = async () => {
+  const temp= "yarab fok eldeka"
+
+  try{
+    const response=await axios.post(`http://localhost:8000/audio/tts?text=${temp}`)
+    console.log(response.data)
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
 
   return (
     <div className='body'>
@@ -31,7 +53,7 @@ const Homepage = () => {
         <ProgressBar/>
         <div className='btns'>
         <button type="submit" >Repeat</button>
-        <button type="submit" className='subBtn'>Play</button>
+        <button  onClick={posttts} className='subBtn'>Play</button>
         <button type="submit" className='subBtn'>Upload</button>
         </div>
        
